b661b58fcc7881d3a3715ff4ef7893cc3674861c
杨润 <yangrun@bytedance.com>
2022-11-04 14:39:54 +0800

    trade: update base image

--- open_api/open_api.py
+++ open_api/open_api.py
@@ -1,5 +1,22 @@
+LOCAL_TOKEN_FILE = '.token.toml'
+
 class OpenAPI:
     def __init__(self, app_id, app_secret):
         self.app_id = app_id
         self.app_secret = app_secret
 
+    @classmethod
+    def from_env(cls):
+        import toml
+        import os
+
+        env_app_id = os.environ.get('APP_ID')
+        env_app_secret = os.environ.get('APP_SECRET')
+        if env_app_id && env_app_secret:
+            return cls(env_app_id, env_app_secret)
+
+        if os.path.exists(LOCAL_TOKEN_FILE):
+            config = toml.load(LOCAL_TOKEN_FILE)
+            return cls(config['app_id'], config['app_secret'])
+
+        raise Exception("could find app_id & app_secret from env or .token.toml")
